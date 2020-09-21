e8a74fd26efda6c4ce0d4020bf9faceeee4c9984
Tao Yuhong <yuhong.tao@intel.com>
2020-09-21 06:06:21 -0400

    HV: pci-vuart: pci based vuart emulation
    Add emulation for pci based vuart device at hv land.
    1. vuart_pci_msi(vdev), vdev can use this API to inject msi to VM
    2. struct pci_vdev_ops vuart_pci_ops, the vdev callbalks for vuart.
    
    How to use
    In misc/vm_configs/scenarios/<SCENARIO>/<BOARD>/pci_dev.c, add pci
    vuart config to vm_pci_devs[] array. For example:
    
    struct acrn_vm_pci_dev_config vm0_pci_devs[] = {
           /* console vuart setting*/
           {
                   .emu_type = PCI_DEV_TYPE_HVEMUL,
                   .vbdf.bits = {.b = 0x00U, .d = 0x04U, .f = 0x00U},
                   .vdev_ops = &vuart_pci_ops,
                   .vbar_base[0] = 0x80001000,	/* mmio bar */
                   .vbar_base[1] = 0x80002000,	/* msix bar */
                   .vuart_idx = 0,
           },
           /* communication vuart setting */
           {
                   .emu_type = PCI_DEV_TYPE_HVEMUL,
                   .vbdf.bits = {.b = 0x00U, .d = 0x05U, .f = 0x00U},
                   .vdev_ops = &vuart_pci_ops,
                   .vbar_base[0] = 0x80003000,
                   .vbar_base[1] = 0x80004000,
                   .vuart_idx = 1,
                   .t_vuart.vm_id = 1U,
                   .t_vuart.vuart_id = 1U,
           },
    }
    
    Tracked-On: #5394
    Signed-off-by: Tao Yuhong <yuhong.tao@intel.com>

---
+++ hypervisor/include/dm/vuart-mcs99xx.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2020 Intel Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef VUART_PCI_H
+#define VUART_PCI_H
+
+#define COM_VENDOR	0x9710U
+#define COM_DEV		0x9900U
+
+extern const struct pci_vdev_ops vuart_pci_ops;
+void trigger_vuart_msi(struct pci_vdev *vdev);
+
+#define pr_vpci(FMT, ...) do { \
+	struct acrn_vm *vm = vpci2vm(vdev->vpci); \
+        pr_warn("vm%d vuart %02x:%02x.%x " FMT, \
+                        vm->vm_id, vdev->bdf.bits.b, vdev->bdf.bits.d, vdev->bdf.bits.f, \
+                        __VA_ARGS__); }while(0)
+
+#endif
